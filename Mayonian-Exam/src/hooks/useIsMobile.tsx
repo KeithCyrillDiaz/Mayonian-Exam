69d8fa52b6e9fca56636cad83f446d67ee341b21
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-21 13:18:41 +0800

    added custom hooks for enabling the website for mobile users only

---
+++ Mayonian-Exam/src/hooks/useIsMobile.tsx
@@ -0,0 +1,31 @@
+import { status } from "../lib"
+import { useState, useEffect } from "react";
+
+export const useIsMobile = () => {
+    const [isMobile, setIsMobile] = useState<boolean>(false);
+    const [isLandscape, setIsLandscape] = useState<boolean>(false);
+    const [status, setStatus] = useState<status>('loading');
+    const [windowSizeY, setWindowSizeY] = useState<number>(1366) //default size of my monitor
+
+    const handleResize = () => { 
+        setIsMobile(window.innerWidth < 768);
+        setIsLandscape(window.innerWidth < 1200);
+        setWindowSizeY(window.innerWidth)
+    };
+
+    useEffect(() => {
+        handleResize();
+        setStatus('idle');
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return {
+        isMobile,
+        isLandscape,
+        status,
+        windowSizeY
+    };
+};
