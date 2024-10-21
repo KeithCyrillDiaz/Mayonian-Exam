8f9750b43896ae803ef3022a9d5289e0dab5fd97
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-21 18:03:23 +0800

    added button types

--- Mayonian-Exam/src/lib/types.tsx
+++ Mayonian-Exam/src/lib/types.tsx
@@ -1,1 +1,18 @@
 export type status = "loading" | "idle";
+
+export type ButtonLabels = 
+| "SEARCH"
+| "START"
+| "NEW"
+| "SLOTS"
+| "LIVE"
+| "JACKPOTS"
+| "SPORTS"
+| "FAVORITES"
+| "INVITE"
+| "CASINO LIVE"
+| "CASHIER";
+
+export type ButtonIconsTypes = {
+    [key in ButtonLabels]: JSX.Element;
+}
