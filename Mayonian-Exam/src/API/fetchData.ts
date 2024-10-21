c6f21bfbe983e8ea6ae2e978c3ff1332f3594fe4
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-21 12:48:01 +0800

    ädded mock api, might change later if needed

--- Mayonian-Exam/src/API/fetchData.ts
+++ Mayonian-Exam/src/API/fetchData.ts
@@ -1,4 +1,16 @@
-
-
-
-
+export const MockGameList = async (succeed: boolean, data: any[]) => {1
+    return await new Promise((resolve) => {
+        setTimeout(()=>{
+            if(succeed) {
+                resolve({
+                    status: "200",
+                    messages: {
+                        code: 0,
+                        message:  "Data Fetch Successfully"
+                    },
+                    data
+                })
+            }
+        }, 3000)
+    })
+}
